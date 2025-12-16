2313067d0c9f68318646a9e3a5ae8bd491cfeb0a
Nadeem Khan <118846447+nadeemkhan9876@users.noreply.github.com>
2025-12-16 12:32:04 +0100

    Ex1

---
+++ function.py
@@ -0,0 +1,20 @@
+# def add_number(num_a,num_b):
+#     print ("Add:",num_a + num_b)
+ 
+
+# result=add_number(5,6)
+# print(result)
+
+# def func(num_1,num_2):
+#     if num_1>=0:
+#         return ("positive")
+#     print (num_2)
+# return  (num_2)
+
+# def function_orzero(n):
+#     if n>0:
+#         return "positive"
+#     return "zero or neg"
+
+print("Hello")
+
